1e2f303c3ec893eaa8cce343cca9a85695d5a1ee
Saivijay57 <korvisaivijay@gmail.com>
2022-01-25 12:56:19 +0530

    added password field in registration page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 // import logo from './logo.svg';
 import './App.css';
 import Registration from './registrationpage/Registration';
+import Pastorders from './pastorderspage/Pastorders';
 
 function App() {
   return (
     <>
-      <Registration/>
+    <Registration/>
+      {/* <Pastorders/> */}
     </>
   );
 }
--- frontend/src/pastorderspage/Pastorders.js
+++ frontend/src/pastorderspage/Pastorders.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import "./Pastorders.css"
+import Loginheader from "./components/Loginheader"
+import Loginfooter from './components/Loginfooter';
 
 export default function Pastorders() {
   return (
       <>
       {/* start coding here */}
+      <Loginheader/>
+      <div className='pastordersbody'>
+      <div className='tooglebar'>
+
+
+      </div>
+      <div className='pastorderbox'></div>
+
+      </div>
+      <Loginfooter/>
+
+
 
 
 
